0f1eb4f29f1164903e76e75cbf99bc561d94f3cc
Jordan Abi <jordan.zeng@hotmail.de>
2023-09-19 11:02:22 +0200

    parametrize tests

--- test_email_validation.py
+++ test_email_validation.py
@@ -27,3 +27,44 @@ class Tests():
         print("\n\n" + str(result))
         assert result == expected
 
+@pytest.mark.parametrize("email", [
+    ("email@example.com")
+    ("firstname.lastname@example.com")
+    ("email@subdomain.example.com")
+    ("firstname+lastname@example.com")
+    ("email@123.123.123.123")
+    ("email@[123.123.123.123]")
+    ('"email"@example.com')
+    ("1234567890@example.com")
+    ("email@example-one.com")
+        ("_______@example.com")
+        ("email@example.name")
+        ("email@example.museum")
+        ("email@example.co.jp")
+        ("firstname-lastname@example.com")
+])
+def test_is_valid_email__gueltige_Adressen(email):
+    assert is_valid_email(email) == True
+
+
+@pytest.mark.parametrize("email", [
+    ("plainaddress")
+    ("#@%^%#$@#$@#.com")
+    ("@example.com")
+    ("Joe Smith <email@example.com>")
+    ("email.example.com")
+    ("email@example@example.com")
+    (".email@example.com")
+    ("email.@example.com")
+    ("email..email@example.com")
+    ("あいうえお@example.com")
+    ("email@example.com(Joe Smith)")
+    ("email@example")
+    ("email@-example.com")
+    ("email@example.web")
+    ("email@111.222.333.44444")
+    ("email@example..com")
+    ("Abc..123@example.com")
+])
+def test_is_valid_email__ungueltige_Adressen(email):
+    assert is_valid_email(email) == False
