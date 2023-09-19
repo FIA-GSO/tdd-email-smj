41fb4b3b6a68afd83f89000059deba4c85a3c6be
Baris Salcan <barissalcan@web.de>
2023-09-19 10:53:05 +0200

    Push initials and layout

--- test_email_validation.py
+++ test_email_validation.py
@@ -2,27 +2,28 @@ import pytest
 from input_validation import *
 
 
-def test_is_valid_mail_1():
-    #TODO: arrange
-    mail = "jordan.zeng@azerbaijan.az"
-    expected = True
+class Tests():
+    def test_is_valid_mail_1(self):
+        #TODO: arrange
+        mail = "jordan.zeng@azerbaijan.az"
+        expected = True
 
-    #TODO: act
-    result = is_valid_email(mail)
+        #TODO: act
+        result = is_valid_email(mail)
 
-    #TODO: assert
-    print("\n\n" + str(result))
-    assert result == expected
+        #TODO: assert
+        print("\n\n" + str(result))
+        assert result == expected
 
+    def test_is_valid_mail_2(self):
+        #TODO: arrange
+        mail = "jordan.zeng*#/\//||@azerbaijan.az"
+        expected = False
 
-def test_is_valid_mail_2():
-    #TODO: arrange
-    mail = "jordan.zeng*#/\//||@azerbaijan.az"
-    expected = False
+        #TODO: act
+        result = is_valid_email(mail)
 
-    #TODO: act
-    result = is_valid_email(mail)
+        #TODO: assert
+        print("\n\n" + str(result))
+        assert result == expected
 
-    #TODO: assert
-    print("\n\n" + str(result))
-    assert result == expected
